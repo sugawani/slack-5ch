fc70fb3b337f7fd1b3648aee4db5f4e300c7d8a6
sugawani <nekorollykk@gmail.com>
2023-08-15 14:27:16 +0900

    fix DO class export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { SlackApp, SlackEdgeAppEnv } from "slack-cloudflare-workers";
 import { SlashCommand } from "slack-edge";
+export { IDCounter } from "./IDCounter";
 
 export interface Env extends SlackEdgeAppEnv {
   POST_CHANNEL_ID: string;
@@ -74,14 +75,3 @@ export default {
     return await app.run(request, ctx);
   },
 };
-
-// 別クラスにすると動かないので同じクラスの中に書く
-export class IDCounter {
-  constructor(private readonly state: DurableObjectState, env: Env) {}
-  async fetch(_request: Request): Promise<Response> {
-      const id: number = await this.state.storage?.get("id") || 0;
-      await this.state.storage?.put("id", id+1)
-
-      return new Response(id.toString());
-  }   
-}
