0c9c98bd59758b007f463805bfcdcc249a95ae03
sugawani <nekorollykk@gmail.com>
2023-08-19 00:38:12 +0900

    fix DO

--- src/IDCounter.ts
+++ src/IDCounter.ts
@@ -1,10 +1,9 @@
-// 別クラスにすると動かないので同じクラスの中に書く
 export class IDCounter {
-    constructor(private readonly state: DurableObjectState, env: Env) {}
+    constructor(private readonly state: DurableObjectState) {}
     async fetch(_request: Request): Promise<Response> {
         const id: number = await this.state.storage?.get("id") || 0;
         await this.state.storage?.put("id", id+1)
-  
+
         return new Response(id.toString());
     }   
-  }
+}
